f8b05c9e005158661e819ee086b7da81920ad8e7
devopsnhc01 <126224064+devopsnhc01@users.noreply.github.com>
2023-02-28 12:54:04 +0530

    Update sum.java

--- amjava/sum.java
+++ amjava/sum.java
@@ -1,11 +1,21 @@
- class sum
+ //sample Jenkins Code
+import java.util.Scanner;
+public class sum
 {
 
- public static void main(String[] args) 
+    public static void main(String[] args) 
 {
-
-  int num1 = 5, num2 = 15,sum;
-  sum = num1+num2;
-  System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
-  }
+        
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        num1 = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        num2 = sc.nextInt();
+        
+        sc.close();
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
 }
