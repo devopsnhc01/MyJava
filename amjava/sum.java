39dc66af2b65a5289bb80fa297690a8bcd769520
devopsnhc01 <126224064+devopsnhc01@users.noreply.github.com>
2023-02-28 13:51:54 +0530

    Update sum.java

--- amjava/sum.java
+++ amjava/sum.java
@@ -9,10 +9,10 @@ public class sum
         int num1, num2, sum;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Number: ");
-        num1 = sc.nextInt();
+        num1 =Integer.parseInt(args[0]);
         
         System.out.println("Enter Second Number: ");
-        num2 = sc.nextInt();
+        num2 = Integer.parseInt(args[0]);
         
         sc.close();
         sum = num1 + num2;
